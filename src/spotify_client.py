f61e2f60be2944b98a5158bad05260da821a70f3
Alfredo De La Torre <alfredodelatorre891@gmail.com>
2026-07-09 20:52:13 -0700

    updating the .gitignore file to dismiss the .env and .cache file, and to update spotify_client module to be able to read the current playing tracks item information

--- src/spotify_client.py
+++ src/spotify_client.py
@@ -6,23 +6,28 @@ Spotify API helper functions.
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
 
-SCOPE = "user-read-currently-playing user-read--playback-state"
+load_dotenv()
+SCOPE = "user-read-currently-playing user-read-playback-state"
 
 def create_spotify_client():
-	return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
+	return spotipy.Spotify(auth_manager=SpotifyOAuth(
+		scope=SCOPE
+		)
+	)
 
 def get_current_track(sp):
 	current_track = sp.current_user_playing_track()
-	if not current_track or not current.get("item"):
+	if not current_track or not current_track.get("item"):
 		return None
-	item = current["item"]
+	item = current_track["item"]
 	return {
-		"id": item["id"]
-		"title": item["name"]
-		"arist": item["arist"][0]["name"]
-		"album": item["album"]["name"]
-		"album_art_url": item["album"]["images"][0]["url"]
+		"id": item["id"], 
+		"title": item["name"], 
+		"artist": item["artists"][0]["name"], 
+		"album": item["album"]["name"], 
+		"album_art_url": item["album"]["images"][0]["url"], 
 		"is_playing": current_track["is_playing"]
 	}
 
